d20a1e079690b397fe108e11f84b974c9f02bc69
badallogical <badallearn@gmail.com>
2023-01-18 09:10:42 +0530

    first commit

--- app/src/main/java/com/example/uitest/stats.kt
+++ app/src/main/java/com/example/uitest/stats.kt
@@ -1,13 +1,18 @@
 package com.example.uitest
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 class stats : Fragment() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,4 +25,48 @@ class stats : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stats, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var lineChart : LineChart = view.findViewById(R.id.chart1);
+
+        // entries
+        val entries = ArrayList<Entry>()
+        entries.add(Entry(1f, 10f))
+        entries.add(Entry(2f, 2f))
+        entries.add(Entry(3f, 7f))
+        entries.add(Entry(4f, 20f))
+        entries.add(Entry(5f, 16f))
+
+        val vl = LineDataSet(entries, "My Type")
+
+        vl.setDrawValues(false)
+        vl.setDrawFilled(true)
+        vl.lineWidth = 3f
+        vl.fillColor = R.color.grey
+        vl.fillAlpha = R.color.red
+
+        //Part5
+        lineChart.xAxis.labelRotationAngle = 0f
+
+//Part6
+        lineChart.data = LineData(vl)
+
+//Part7
+        lineChart.axisRight.isEnabled = false
+        lineChart.xAxis.axisMaximum = 0.1f
+
+//Part8
+        lineChart.setTouchEnabled(true)
+        lineChart.setPinchZoom(true)
+
+//Part9
+        lineChart.description.text = "Days"
+        lineChart.setNoDataText("No forex yet!")
+
+//Part10
+        lineChart.animateX(1800, Easing.EaseInExpo)
+    }
+
 }
